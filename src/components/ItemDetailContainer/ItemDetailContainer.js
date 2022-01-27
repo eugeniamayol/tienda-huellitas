656d7ba802443f4c7e457d2c4af17b578c9314c3
Eugenia Mayol <eugenia.mayol@hotmail.com>
2022-01-27 20:59:02 -0300

    Item Collection arreglo

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -2,7 +2,7 @@ import {ItemDetail} from '../ItemDetail/ItemDetail.js';
 import { useEffect, useState } from "react";
 import { traerProductos } from '../../helpers/mocks';
 import { useParams } from "react-router-dom";
-import { getDoc, getFirestore, doc } from 'firebase/firestore';
+import { getDoc, getFirestore, doc, where } from 'firebase/firestore';
 
 
 export const ItemDetailContainer = () => {
@@ -11,12 +11,14 @@ export const ItemDetailContainer = () => {
   const { idDetalle } = useParams();
 
   useEffect(() =>{
+    if(idDetalle){
+      const db = getFirestore()
+      const queryProd = doc((db, 'items', 'id'),
+      where('id', '==', idDetalle));
+      getDoc(queryProd)
+          .then(res => {setProducto( { id: res.id, ...res.data() } )});
+          }
 
-     const db = getFirestore()
-     const queryProd = doc(db, 'items', 'id');
-     getDoc(queryProd)
-         .then((res) => {setProducto( { id: res.id, ...res.data() });
-  });
     // traerProductos
     //     .then(res => setProducto(res.find(prod => prod.id === idDetalle)))
     //     .catch(err => console.log (err))
